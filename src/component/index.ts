0d29d66ee90e704eb5f44cfa26efab114ec728c8
Peng Wan <wanpeng@mail.com>
2021-01-18 12:48:09 +0800

    feature: add DataTable and Typography exports

--- src/component/index.ts
+++ src/component/index.ts
@@ -4,4 +4,6 @@ export * from './frame';
 export * from './http';
 export * from './prompt';
 export * from './router';
+export * from './table';
+export * from './typography';
 export * from './common-component';
